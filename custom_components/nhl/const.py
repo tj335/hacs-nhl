c430da982fad26e24b196a38a57e68ba333221c5
tj335 <105184198+tj335@users.noreply.github.com>
2022-05-14 09:17:23 -0400

    New API endpoints and bumped version
    Replaced API_ENDPOINT with API_SCOREBOARD_ENDPOINT and API_TEAM_ENDPOINT.
    Bumped version to 0.2

--- custom_components/nhl/const.py
+++ custom_components/nhl/const.py
@@ -1,5 +1,6 @@
 # API
-API_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard"
+API_SCOREBOARD_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard"
+API_TEAM_ENDPOINT = "https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/teams/"
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
 
 # Config
@@ -13,8 +14,8 @@ DEFAULT_TIMEOUT = 180
 
 # Misc
 TEAM_ID = ""
-VERSION = "0.1"
-ISSUE_URL = "https://github.com/zacs/ha_nfl"
+VERSION = "0.2"
+ISSUE_URL = "https://github.com/tj335/hacs_nhl"
 DOMAIN = "nhl"
 PLATFORM = "sensor"
 ATTRIBUTION = "Data provided by ESPN"
